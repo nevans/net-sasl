a1a61f2d4c17853182f7267b424ef3ec9eae01c7
nick evans <nicholas.evans@gmail.com>
2022-05-21 14:06:39 -0400

    Formatting the DIGEST-MD% test
    If we're splitting lines, it's more readable to go all the way and split
    it for each k/v pair.  :)

--- test/net/sasl_test.rb
+++ test/net/sasl_test.rb
@@ -30,17 +30,29 @@ class Net::SASLTest < Test::Unit::TestCase
   test "DIGEST-MD5 authenticator" do
     auth = Net::SASL.authenticator("DIGEST-MD5", "cid", "password", "zid")
     assert_match(
-      #Regexp.new("nonce=\"OA6MG9tEQGm2hh\",username=\"cid\",realm=\"somerealm\"," \
-      #"cnonce=\"[^\"]+\"," \
-      #"digest-uri=\"imap/somerealm\",qop=\"auth\",maxbuf=65535,nc=00000001," \
-      #"charset=utf-8,authzid=\"zid\",response=
-		/\Anonce="OA6MG9tEQGm2hh",username="cid",realm="somerealm",
-         cnonce="[a-f0-9]+",digest-uri="imap\/somerealm",qop="auth",maxbuf=65535,
-         nc=00000001,charset=utf-8,authzid="zid",
-         response=[a-f0-9]+\Z/x,
+      %r{\A
+        nonce="OA6MG9tEQGm2hh",
+        username="cid",
+        realm="somerealm",
+        cnonce="[a-f0-9]+",
+        digest-uri="imap/somerealm",
+        qop="auth",
+        maxbuf=65535,
+        nc=00000001,
+        charset=utf-8,
+        authzid="zid",
+        response=[a-f0-9]+
+      \Z}x,
       auth.process(
-        "realm=\"somerealm\",nonce=\"OA6MG9tEQGm2hh\",qop=\"auth\"," \
-        "charset=utf-8,algorithm=md5-sess"))
+        %w[
+          realm="somerealm"
+          nonce="OA6MG9tEQGm2hh"
+          qop="auth"
+          charset=utf-8
+          algorithm=md5-sess
+        ].join(",")
+      )
+    )
   end
 
 end
